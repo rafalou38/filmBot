ad1ebd935c71ca19eadb866c649098e3db699471
Rafael <rafael.martinezcalvo@orange.fr>
2021-11-12 19:47:35 +0100

    media queries for question edit

--- adminPanel/src/render/lib/question.svelte
+++ adminPanel/src/render/lib/question.svelte
@@ -14,8 +14,18 @@
 <div class="question">
     <Card variant="outlined" padded>
         <div class="content">
-            <Textfield bind:value={question.question} variant="filled" label="Question" />
-            <Textfield bind:value={question.answer} variant="filled" label="Réponse" />
+            <Textfield
+                bind:value={question.question}
+                class="q-edit"
+                variant="filled"
+                label="Question"
+            />
+            <Textfield
+                bind:value={question.answer}
+                class="q-edit"
+                variant="filled"
+                label="Réponse"
+            />
             <Button variant="raised" class="red small-cent" on:click={() => dispatch("remove")}>
                 <Icon class="material-icons">delete</Icon>
                 <Label>supprimer</Label>
@@ -34,5 +44,13 @@
         width: max-content;
         margin-left: auto;
         margin-right: auto;
+    }
+    @media (min-width: 900px) {
+        .content {
+            flex-direction: row;
+        }
+        :global(.q-edit) {
+            flex-grow: 1;
+        }
     }
 </style>
