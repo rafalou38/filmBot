eaed71cb226c75ee5b648aa0ed2a13a8427c2746
Rafael <rafael.martinezcalvo@orange.fr>
2021-11-12 12:29:30 +0100

    remove build folder

--- adminPanel/electron/database/api/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.lean = void 0;
-function lean(doc) {
-    const id = doc.id;
-    return Object.assign({ id }, doc.toObject({ getters: true, virtuals: true }));
-}
-exports.lean = lean;
